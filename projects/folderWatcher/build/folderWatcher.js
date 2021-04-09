243c38958af365b448629d9651ded7c4d6534594
Nikita Krasheninnik <krasheninnik99@gmail.com>
2021-04-09 15:27:16 +0700

    improve folder watcher

--- projects/folderWatcher/build/folderWatcher.js
+++ projects/folderWatcher/build/folderWatcher.js
@@ -3,18 +3,29 @@ var Obserser = require("./services/observer");
 var redis = require("redis");
 var redisClient = redis.createClient();
 redisClient.on("error", function (error) {
-    console.error(error);
+  console.error(error);
 });
 var obserser = new Obserser();
 var sourceFolder = "files/to_process";
 var storageFolder = "files/processed";
 var sequentialFileName = 0;
 obserser.on("file-added", function (msg) {
-    // add filePath in message query
-    redisClient.lpush("filenamequery", msg.filePath);
-    // send signal, that there new file
-    redisClient.publish("alarmchannel", sequentialFileName++);
-    // log:
-    console.log("[" + new Date().toLocaleString() + "] " + msg.filePath + " was added in processing query");
+  // set file processing status to "await processing"
+  redisClient.set(msg.filePath, -1);
+  // add filePath in message query
+  redisClient.lpush("filenamequery", msg.filePath);
+  // send signal, that there new file
+  redisClient.publish("alarmchannel", sequentialFileName++);
+  // send filename to monitoring part:
+  redisClient.publish("monitoringChannel", msg.filePath);
+
+  // log:
+  console.log(
+    "[" +
+      new Date().toLocaleString() +
+      "] " +
+      msg.filePath +
+      " was added in processing query"
+  );
 });
 obserser.watchFolder(sourceFolder, storageFolder);
