7537294ef3a7a79d4d341767ca88d6d9098e2587
Nikita Krasheninnik <krasheninnik99@gmail.com>
2021-04-12 11:25:55 +0700

    front: comment redis access from worker. met some problems...

--- projects/monitoring front/src/workers/filenamelistener.worker.js
+++ projects/monitoring front/src/workers/filenamelistener.worker.js
@@ -1,4 +1,4 @@
-import redis from "redis";
+//import redis from "redis";
 
 /* eslint-disable-next-line no-restricted-globals */
 self.addEventListener("message", startListening);
@@ -10,6 +10,7 @@ console.log("hello from worker");
 function startListening(event) {
   console.log("filenameListenerWorker.js has been started");
 
+  /*  // redis don't wanna work in browser in the easy way :(
   const redisClient = redis.createClient();
   redisClient.on("error", function (error) {
     console.error(error);
@@ -23,6 +24,6 @@ function startListening(event) {
     console.log(`got filename ${message}`);
     this.postMessage(message);
   });
-
+  */
   this.postMessage("It is answer from the worker");
 }
